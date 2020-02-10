6bae9b975f672c3b317aa8c9c2341d702b0ff0c0
Afrasinei Serban <afrasinei.serban@gmail.com>
2020-02-10 21:04:27 +0200

    learning to work with buttons

---
+++ app/src/main/java/com/example/scouting/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.scouting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText1, editText2;
+    private Button buttonSum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    private void addListenerOnButton() {
+        editText1 = (EditText) findViewById(R.id.editText1);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        buttonSum = (Button) findViewById(R.id.button);
+
+        buttonSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value1 = editText1.getText().toString();
+                String value2 = editText2.getText().toString();
+
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
+
+                
+                int sum = a + b;
+
+                Toast.makeText(getApplicationContext(), String.valueOf(sum), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
